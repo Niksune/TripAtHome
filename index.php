945164a8c9372355f06739cdce470da7b3ebe555
Nicolas Benoit <nico.benoit@gmail.com>
2015-02-02 11:03:44 +0100

    Add the index

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+
+<html>
+
+<head>
+	<title>Index</title>
+	<style type="text/css">
+    </style>
+	
+	<script type="text/javascript">
+		function main() {
+
+			
+		}
+	</script>
+</head>
+<body onload="main();">
+
+</body>
+
+</html>
