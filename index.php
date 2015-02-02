e2a6b1ef0765ed6bf79e21cbc5a0ea206896b5f1
Nicolas Benoit <nico.benoit@gmail.com>
2015-02-02 12:07:10 +0100

    Add a basic CSS

--- index.php
+++ index.php
@@ -4,16 +4,23 @@
 
 <head>
 	<title>Index</title>
-	
-
-
+	<link rel="stylesheet" type="text/css" href="style.css">
 </head>
 
 <body onload="main();">
 
-	<div id="content">
-		<span id="BrokenDreams">Broken Dreams : <span id="nbBD">0</span></span>
-
+	<div id="board">
+		<span id="BrokenDreams">Broken Dreams : <span id="nbBD">0</span></span><br/>
+		<span id="Hopes" style="display:none;">Hopes : <span id="nbHopes">0</span></span>
+	</div>
+	<div id="messageBoard">
+		<span id="Messages">You're doing all good.</span>
+	</div>
+	<div id="actions">
+		<span id="BDBoostSpan" style="display:none;" ><button id="BDBoostBut" onclick="BDBoost()">Boost Broken Dreams Production</button> (cost : <span id="BDBoostPrice">0</span> Broken Dreams)</span><br/>
+		<span id="buyHopeSpan" style="display:none;" ><button id="buyHopeBut" onclick="buyHope()">Buy a Hope</button> (cost : <span id="buyHopePrice">0</span> Broken Dreams)</span>
+		
+	</div>
 	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
 	<script src="main.js"></script>
 </body>
--- main.js
+++ main.js
@@ -1,16 +1,75 @@
+//Global Variables
 var brokenDreams = 0;
+var hopes = 0;
 var incrementationBD = 1;
+var incrementationHopes = 0;
+var BDBoostPrice = 5;
+var buyHopePrice = 10;
 
+//Summon main features
+//Print prices
 function main () {
 
+	$('#BDBoostPrice').html(BDBoostPrice);
+	$('#buyHopePrice').html(buyHopePrice);
 	incrementBD();
 
 }
 
+//Autocall every second, update ressources and makes options appear
 function incrementBD () {
 
 	brokenDreams += incrementationBD;
-	$('#nbBD').html(brokenDreams);
+	hopes += incrementationHopes;
+	updateRessources();
+	if(brokenDreams >= 5)
+		$('#BDBoostSpan').show();
+	if(brokenDreams >= 10)
+		$('#buyHopeSpan').show();
 	setTimeout('incrementBD()',1000);
+
+}
+
+//Refresh the display of ressources
+function updateRessources () {
+
+	$('#nbBD').html(brokenDreams);
+	$('#nbHopes').html(hopes);	
+}
+
+//In case user wants to buy for too expensive
+function cantPay () {
+
+	$('#Messages').html("You don't have the ressources !");
+
+}
+
+//Boosts BrokenDreams production
+function BDBoost () {
+
+	if(brokenDreams<5)
+		cantPay();
+	else{
+		brokenDreams -= 5;
+		incrementationBD += 1;
+		$('#Messages').html("Gather faster mankind's Broken Dreams !");
+	}
+	
+	updateRessources();
+
+}
+
+//Buys an hope
+function buyHope () {
+
+	if(brokenDreams<10)
+		cantPay();
+	else{
+		$('#Hopes').show();
+		hopes += 1;
+		$('#Messages').html("With enough Broken Dreams, you can find an Hope ! ");
+	}
+	
+	updateRessources();
 
 }
