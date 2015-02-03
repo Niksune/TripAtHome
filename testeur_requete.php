853b1af7567f64735dcf9e75bd32bf131071bfc8
Nicolas Benoit <nico.benoit@gmail.com>
2015-02-03 17:39:04 +0100

    Request's tester generates a file with the number of pictures matching with the criterias

--- testeur_requete.php
+++ testeur_requete.php
@@ -2,7 +2,35 @@
 
 	include('fonctions.php');
 
-	//Preparer la requete 
+	
+	/* A DEFINIR A LA MAIN */
+	$character = "Sonic_The_Hedgehog";
+	$arrayOfCriterias = [
+						[$character, "rating:s", "solo"],
+						[$character, "rating:s", "clothing", "solo"],
+						[$character, "rating:s", "nude", "solo"],
+						[$character, "rating:e", "nude", "-sex", "solo"]
+						];
+	
+	$file = fopen('Personnages\\'.$character.'.txt', 'w+'); 
+	
+	//$criterias contient un array avec une serie de criteres
+	foreach ($arrayOfCriterias as $criterias)
+	{
+		fputs($file, $criterias[0]);
+		foreach ($criterias as $value)
+		{
+			if($value == $criterias[0])
+				continue;
+			fputs($file, ' + '.$value);
+		}
+		$request = requestCreator($criterias);
+		$retourChariot = '
+';
+		fputs($file,' : '.howMany($request).''.$retourChariot);
+	}
+	echo "fini";
+	/*//Preparer la requete 
 	$criterias = ["sonic_the_hedgehog", "rating:s", "solo"];
 	
 	$request = requestCreator($criterias);
@@ -11,6 +39,6 @@
 	
 	echo howMany($request);
 	
-	echo "fini";
+	echo "fini";*/
 	
 ?>
