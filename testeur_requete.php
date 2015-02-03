c5dd8c4b916aa1970100bf71775f6842b10aaa17
Nicolas Benoit <nico.benoit@gmail.com>
2015-02-03 16:09:19 +0100

    Separate fonctions

--- testeur_requete.php
+++ testeur_requete.php
@@ -1,17 +1,11 @@
 <?php
 
-	//Preparer la requete (forme : https://e621.net/post/index.xml?tags=sonic_the_hedgehog+gay+rating:safe)
-	$criterias = ["sonic_the_hedgehog", "rating:s", "gay"];
+	include('fonctions.php');
+
+	//Preparer la requete 
+	$criterias = ["sonic_the_hedgehog", "rating:s", "solo"];
 	
-	$request = 'https://e621.net/post/index.xml?tags='.$criterias[0];
-	
-	foreach ($criterias as $value) {
-	
-		if($value == $criterias[0])
-			continue;
-			
-		$request = $request.'+'.$value;
-	}
+	$request = requestCreator($criterias);
 	
 	echo $request;
 	
